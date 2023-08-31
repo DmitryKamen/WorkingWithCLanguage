6c15634283553c7b7dcc040bc551ad11f5e816cc
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-08-31 20:40:19 +0200

    Домашняя работа № 1 Задание 1

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача № 1
+int a1 = 5;
+int b1 = 7;
+int max1 = 0;
+int min1 = 0;
+
+if (a1 > b1)
+{
+    max1 = a1;
+    min1 = b1;
+}
+else
+{
+    max1 = b1;
+    min1 = a1;
+}
+Console.Write("max1 = ");
+Console.WriteLine(max1);
+Console.Write("min1 = ");
+Console.WriteLine(min1);
+
+int a2 = 2;
+int b2 = 10;
+int max2 = 0;
+int min2 = 0;
+
+if (a2 > b2)
+{
+    max2 = a2;
+    min2 = b2;
+}
+else
+{
+    max2 = b2;
+    min2 = a2;
+}
+Console.Write("max2 = ");
+Console.WriteLine(max2);
+Console.Write("min2 = ");
+Console.WriteLine(min2);
+
+int a3 = -9;
+int b3 = -3;
+int max3 = 0;
+int min3 = 0;
+
+if (a3 > b3)
+{
+    max3 = a3;
+    min3 = b3;
+}
+else
+{
+    max3 = b3;
+    min3 = a3;
+}
+Console.Write("max3 = ");
+Console.WriteLine(max3);
+Console.Write("min3 = ");
+Console.WriteLine(min3);
