906be05d2bdc483f8a4e779f4c3e1bc89383fe03
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-01 14:31:49 +0200

    Домашняя работа № 1 Задание 4

---
+++ HomeWork5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание № 4
+
+int N1 = 1;
+while ( N1 < 6 )
+{
+    if( N1 % 2 == 0)
+    {
+        Console.WriteLine(N1);
+    }
+N1 = N1 + 1;
+}
+Console.WriteLine("Чётные числа");
+
+int N2 = 1;
+while ( N2 < 9 )
+{
+    if( N2 % 2 == 0)
+    {
+        Console.WriteLine(N2);
+    }
+N2 = N2 + 1;
+}
+Console.Write("Чётные числа");
