49acee61ad4f8f8c6339f4536790245df624795a
Каменев Дмитрий <dkamenev1982@gmail.com>
2023-09-01 10:14:27 +0200

    Домашняя работа № 1 Задание 3

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача № 3
+int a = 4;
+if ( a % 2 == 0 )
+{
+    Console.Write(a);
+    Console.WriteLine(" - ДА число четное");
+}
+
+int b = -3;
+if ( b % 2 == 0 )
+{
+    Console.Write(b);
+    Console.WriteLine(" - ДА число четное");
+}
+else
+{
+    Console.Write(b);
+    Console.WriteLine(" - НЕТ число нечетное");
+}
+
+int с = 7;
+if ( с % 2 == 0 )
+{
+    Console.Write(с);
+    Console.WriteLine(" - ДА число четное");
+}
+else
+{
+    Console.Write(с);
+    Console.WriteLine(" - НЕТ число нечетное");
+}
